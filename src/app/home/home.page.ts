3ed36f22f8b4d1aba51062d9f5d7da10b6a6e39f
MaurizioBonanno <mauriziobonanno43@gmail.com>
2019-07-08 22:39:39 +0200

    service helper riconosce web e sistema nativo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HelperService } from './../providers/helper.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private helperService: HelperService) {
+    console.log('is platorm native', this.helperService.isNativePlatfomr());
+  }
 
 }
--- src/app/providers/helper.service.ts
+++ src/app/providers/helper.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelperService {
 
-  constructor() { }
+  constructor(private platform: Platform) { }
+
+  isNativePlatfomr() {
+    return (this.platform.is('cordova') || this.platform.is('capacitor'))
+  }
 }
