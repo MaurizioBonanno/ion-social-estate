4e98f61522ede5de23bef7768e575297843d9f43
MaurizioBonanno <mauriziobonanno43@gmail.com>
2019-07-10 18:37:05 +0200

    aggiunti toast nativi e helper per riconoscere dvice nativi

--- src/app/providers/widget-util.service.ts
+++ src/app/providers/widget-util.service.ts
@@ -1,19 +1,40 @@
+import { HelperService } from './helper.service';
 
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WidgetUtilService {
 
-  constructor(private toastService: ToastrService) { }
+  constructor(private toastService: ToastrService, private toastController: ToastController, 
+              private helperService: HelperService) { }
 
   showToast(message, title){
-    if (title === 'SUCCESS') {
-    this.toastService.success(message, title);
-    } else if ( title === 'ERROR'){
-      this.toastService.error(message, title);
+    if (this.helperService.isNativePlatfomr) {
+
+      this.showNativeToast(message);
+
+    } else {
+          if (title === 'SUCCESS') {
+              this.toastService.success(message, title);
+          } else if ( title === 'ERROR'){
+              this.toastService.error(message, title);
+         }
     }
+
   }
+
+  async showNativeToast(message){
+    const toast = await this.toastController.create({
+      // tslint:disable-next-line:object-literal-shorthand
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+
 }
