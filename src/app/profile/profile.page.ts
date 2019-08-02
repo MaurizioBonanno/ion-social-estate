8499a27f87b2ff768635653d8e164fd768f1c9bc
MaurizioBonanno <mauriziobonanno43@gmail.com>
2019-08-02 14:24:41 +0200

    profilo preso da firestore

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -13,12 +13,13 @@ export class ProfilePage implements OnInit {
   profileInfo: any = null;
   profileAvaileble = false;
 
-
+  utente: any = null;
 
   constructor(private firebaseAuthService: FirebaseAuthService,
               private widgteUtilService: WidgetUtilService,
               private pfs: ProfiliService) {
-    this.getProfile();
+   // this.getProfile();
+    this.getUtente();
   }
 
   getProfile() {
@@ -37,6 +38,22 @@ export class ProfilePage implements OnInit {
       this.widgteUtilService.showToast(error.message , 'ERROR');
       this.profileAvaileble = true;
     });
+  }
+
+   getUtente() {
+    try {
+     this.firebaseAuthService.getLoggedInUser().subscribe( us => {
+       if (us) {
+      this.profileInfo = us.toJSON();
+      console.log('ID---------', this.profileInfo.uid);
+      const result = this.pfs.getProfiloById(this.profileInfo.uid);
+      this.utente = result;
+       }
+     });
+
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   ngOnInit() {
